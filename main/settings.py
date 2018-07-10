e9ce08fe8efe236433d7a723197734f7a0d222a9
Unknown <jeanb.rocher@gmail.com>
2018-07-10 17:45:32 +0200

    Fix collectstatic settings

--- main/settings.py
+++ main/settings.py
@@ -64,7 +64,7 @@ MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
 STATIC_ROOT = os.path.join(DATA_DIR, 'static')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'main', 'static', 'contact_form'),
+    os.path.join(BASE_DIR, 'main', 'static'),
 )
 SITE_ID = 1
 

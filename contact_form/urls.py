b788aba0f8288c4c95daab963eabca32f8c969f8
Unknown <jeanb.rocher@gmail.com>
2018-09-04 08:36:38 +0200

    Comment out the url rendering the form default template

--- contact_form/urls.py
+++ contact_form/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.contact_form, name='contact_form'),
+    # url(r'^$', views.contact_form, name='contact_form'),
     url(r'^/new_potential/$', views.new_potential, name='new_potential'),
 ]

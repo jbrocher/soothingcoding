93e8cfb585672bc00268783093414aa72ddc3359
Unknown <jeanb.rocher@gmail.com>
2018-09-04 10:46:13 +0200

    Set cookie ssecure settings

--- main/settings.py
+++ main/settings.py
@@ -26,8 +26,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 
-
-ALLOWED_HOSTS = []
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 ALLOWED_HOSTS = ['*']
 
 

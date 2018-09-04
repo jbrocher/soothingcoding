fa07f8e4b0c98faec6154801a0261cbe6f2162c8
Unknown <jeanb.rocher@gmail.com>
2018-09-04 09:41:51 +0200

    Fix adminn site

--- contact_form/admin.py
+++ contact_form/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from contact_form.models import Potential
 
 # Register your models here.
-admin.site.register(Potential)
+
+
+@admin.register(Potential)
+class PotentialAdmin(admin.ModelAdmin):
+    list_display = ('project_type', 'project_desc', 'contact_name', 'contact_email', 'contact_name', 'contact_company', 'contact_desc', 'dt_contact')

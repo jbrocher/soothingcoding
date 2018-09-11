ffb6f9cdfc87521be44c4145cfde252dc905c959
Unknown <jeanb.rocher@gmail.com>
2018-09-11 17:47:46 +0200

    Fix extension model

--- snippets/models.py
+++ snippets/models.py
@@ -4,13 +4,12 @@ from cms.extensions.extension_pool import extension_pool
 
 
 class SnippetExtension(PageExtension):
-    name = models.CharField(max_length=255)
     snippets = models.ManyToManyField('Snippet')
 
     def copy_relations(self, oldinstance, language):
         for snippet in oldinstance.snippets.all():
             snippet.pk = None
-            snippet.mypageextension = self
+            snippet.snippetextension = self
             snippet.save()
 
 
